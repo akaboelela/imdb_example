b5f0fbdb4a0b17bc3e77b6e039c2cafe93e68095
Raghu Betina <raghu@firstdraft.com>
2018-01-03 16:57:27 +0000

    Whitelist columns in Direction for ActiveAdmin

--- app/admin/direction.rb
+++ app/admin/direction.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Direction do
 
+ permit_params :director_id, :actor_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
